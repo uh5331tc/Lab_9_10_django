0658bd768ac773c0fe135e9e6985bb5a3e85c2e8
Noelle sdfds <you@gmail.com>
2022-11-03 10:15:27 -0500

    finsished video 3

--- wishlist/travel_wishlist/urls.py
+++ wishlist/travel_wishlist/urls.py
@@ -1,10 +1,10 @@
 ##new urls.py
-
-from django import path
+from django.urls import path, include
 from . import views
 
 #request a path to the homepage and hanle with function
 urlpatterns = [
     path('', views.place_list, name='place_list'),
+    path('', include('travel_wishlist.urls'))
 
 ]
--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -3,4 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 
 def place_list(request):
-    return render (request, 'travel_wishlist/wishlist.html')
+    places = Place.objects.filter(visited=False).order_by('name')
+
+    return render (request, 'travel_wishlist/wishlist.html', {'places': places})
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
