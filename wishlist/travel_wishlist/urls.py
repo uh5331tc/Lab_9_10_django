a65ccefd58b437f387c18e856f7a1e3ce1286a42
Noelle sdfds <you@gmail.com>
2022-11-03 10:49:46 -0500

    finished video 4

--- wishlist/travel_wishlist/urls.py
+++ wishlist/travel_wishlist/urls.py
@@ -1,10 +1,11 @@
 ##new urls.py
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 #request a path to the homepage and hanle with function
 urlpatterns = [
     path('', views.place_list, name='place_list'),
-    path('', include('travel_wishlist.urls'))
+    path('about', views.about, name='about')
+
 
 ]
--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,15 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .models import Place
+from .forms import NewPlaceForm
 # Create your views here.
 
 def place_list(request):
+
+    #create new place
+    form = NewPlaceForm(request.POST)  #creating a form from the data thats in the request
+    place = form.save()  #creating a model object from form
+    if form.is_valid():  # validation agaisnst DB constraints
+        place.save()   # saves place to the DB 
+        return redirect('place_list')  #reloads homepage
+
+
     places = Place.objects.filter(visited=False).order_by('name')
+    new_place_form = NewPlaceForm()
+    return render (request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
 
-    return render (request, 'travel_wishlist/wishlist.html', {'places': places})
-
-    
-
-
+def about(request):
+    author = 'Noelle'
+    about = 'A website to create a list of places to visit'
+    return render(request, 'travel_wishlist/about.html', {'author': author, 'about': about})
 
 
 
